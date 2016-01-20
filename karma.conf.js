3ecc9bcc596c69912b0a822e84cb89d833926813
rubenhazelaar <r.hazelaar@surifleetmanagement.com>
2016-01-20 20:39:09 -0300

    chore: Adjusted .travis.yml, package.json & karma.conf.js to get Chrome running on travis ci

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Fri Jan 08 2016 21:45:44 GMT-0300 (SA Eastern Standard Time)
 
 module.exports = function (config) {
-    config.set({
+    var cfg = {
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
@@ -56,7 +56,14 @@ module.exports = function (config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         //browsers: ['ChromeCanary', 'Firefox', 'Safari', 'IE'],
-        browsers: ['ChromeCanary'],
+        browsers: ['ChromeCanary', 'Chrome'],
+
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
@@ -107,5 +114,11 @@ module.exports = function (config) {
             noInfo: true
             //silent: true
         }
-    })
+    }
+
+    if (process.env.TRAVIS) {
+        cfg.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(cfg);
 };

274d942d175082c4cbb1541a8a85f369a2d934dc
rubenhazelaar <ruben.hazelaar@gmail.com>
2016-02-18 18:18:33 -0300

    feat: Expose utility functions
    closes #11

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import addExtension from './dom/extension.js';
 import replace from './dom/replace.js';
 import Router, { Route, IndexRoute } from './router/Router.js';
 import Link from './router/Link.js';
-import debounce from './utils/debounce.js';
-import throttle from './utils/throttle.js';
 import Fetch from './xhr/Fetch.js';
 import action, { Do } from './action/action.js';
 import AsyncAction from './action/AsyncAction.js';
 import reaction from './action/reaction.js';
+import capitalize from './utils/capitalize.js';
+import debounce from './utils/debounce.js';
+import isFunction from './utils/isFunction.js';
+import isObject from './utils/isObject.js';
+import merge from './utils/merge.js';
+import throttle from './utils/throttle.js';
 
 export default Component;
 
@@ -21,10 +25,14 @@ export {
     replace,
     Router, Route, IndexRoute,
     Link,
-    debounce,
-    throttle,
     Fetch,
     action, Do,
     AsyncAction,
-    reaction
+    reaction,
+    capitalize,
+    debounce,
+    isFunction,
+    isObject,
+    merge,
+    throttle
 };
